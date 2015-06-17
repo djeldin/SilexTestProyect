feada98d8c887b6cd9b2b3702a96c8469a186568
Eldin SLopez <djeldin007@msn.com>
2015-06-17 09:09:48 -0600

    Update table.sql

--- TS/table.sql
+++ TS/table.sql
@@ -1,7 +1,7 @@
-'CREATE TABLE `Simple` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(30) NOT NULL,
-  `precio` float NOT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8'
+CREATE TABLE Simple (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  descripcion varchar(30) NOT NULL,
+  precio float NOT NULL,
+  reg_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB
